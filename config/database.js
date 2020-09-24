709efa9ee9c92cf9cf4961badff254d10484742f
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-23 21:24:04 -0400

    update

--- config/database.js
+++ config/database.js
@@ -3,4 +3,4 @@ module.exports = {
 
   'url': 'mongodb+srv://demo:demo@cluster0.84ovu.mongodb.net/bookdb?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
   'dbName': 'bookdb'
-}; //test
+};
